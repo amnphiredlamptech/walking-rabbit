97081a578671d693a80505422e8c483b02356560
man <amnphi@redlamp.tech>
2022-10-18 15:48:05 +0700

    test module

---
+++ dist/types.d.ts
@@ -0,0 +1,13 @@
+
+import { ModuleOptions, ModuleHooks, ModuleRuntimeConfig, ModulePublicRuntimeConfig } from './module'
+
+declare module '@nuxt/schema' {
+  interface NuxtConfig { ['sample']?: Partial<ModuleOptions> }
+  interface NuxtOptions { ['sample']?: ModuleOptions }
+  interface NuxtHooks extends ModuleHooks {}
+  interface RuntimeConfig extends ModuleRuntimeConfig {}
+  interface PublicRuntimeConfig extends ModulePublicRuntimeConfig {}
+}
+
+
+export { default } from './module'
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   modules: [
-    ['./module', { token: '123' }],
+    ['./src/module.js', { token: '123' }],
   ]
 })
