8c634e0950fe6f56bca942c3cee1da4864e6c393
man <amnphi@redlamp.tech>
2022-10-18 15:58:34 +0700

    test module

--- dist/module.mjs
+++ dist/module.mjs
@@ -1,6 +1,6 @@
 import jiti from "file:///Users/man/Desktop/workspace/microservice/vue-hello-world-simple-nuxt/node_modules/jiti/lib/index.js";
 
 /** @type {import("/Users/man/Desktop/workspace/microservice/vue-hello-world-simple-nuxt/src/module")} */
-const _module = jiti(null, { interopDefault: true, esmResolve: true })("/Users/man/Desktop/workspace/microservice/vue-hello-world-simple-nuxt/src/module.js");
+const _module = jiti(null, { interopDefault: true, esmResolve: true })("/Users/man/Desktop/workspace/microservice/vue-hello-world-simple-nuxt/src/module.ts");
 
 export default _module;
