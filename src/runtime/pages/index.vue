0c32749b45aa86e53fea2a54fa8ab374bfb12b0c
man <amnphi@redlamp.tech>
2022-10-19 18:46:36 +0700

    test

--- src/runtime/pages/index.vue
+++ src/runtime/pages/index.vue
@@ -1,10 +1,21 @@
 <template>
   <div>
+    ======
+    <br />
     Nuxt module playground!
     <Counter />
+    <br />
+    {{ resUser }}
+    <br />
+    ======
   </div>
 </template>
 
 <script setup>
-import Counter from '../components/Counter.vue';
+import { useFetch } from "#imports";
+import Counter from "../components/Counter.vue";
+
+const { data: resUser } = await useFetch(
+  `https://gorest.co.in/public/v1/users`
+);
 </script>
